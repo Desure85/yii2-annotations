edff06991b0e1c051c6cb855c252e3355f575ea7
d.egorov <d.egorov@smileexpo.ru>
2019-04-29 14:47:20 +0300

    Initial

---
+++ src/AnnotationCache.php
@@ -0,0 +1,100 @@
+<?php
+
+use yii\caching\CacheInterface;
+
+class AnnotationCache implements Doctrine\Common\Cache\Cache
+{
+
+    private $yiiCache;
+
+    /**
+     * AnnotationCache constructor.
+     * @param $yiiCache
+     */
+    public function __construct(CacheInterface $yiiCache)
+    {
+        $this->yiiCache = $yiiCache;
+    }
+
+    /**
+     * Fetches an entry from the cache.
+     *
+     * @param string $id The id of the cache entry to fetch.
+     *
+     * @return mixed The cached data or FALSE, if no cache entry exists for the given id.
+     */
+    public function fetch($id)
+    {
+        return $this->yiiCache->get($id);
+    }
+
+    /**
+     * Tests if an entry exists in the cache.
+     *
+     * @param string $id The cache id of the entry to check for.
+     *
+     * @return bool TRUE if a cache entry exists for the given cache id, FALSE otherwise.
+     */
+    public function contains($id)
+    {
+        return $this->yiiCache->exists($id);
+    }
+
+    /**
+     * Puts data into the cache.
+     *
+     * If a cache entry with the given id already exists, its data will be replaced.
+     *
+     * @param string $id The cache id.
+     * @param mixed $data The cache entry/data.
+     * @param int $lifeTime The lifetime in number of seconds for this cache entry.
+     *                         If zero (the default), the entry never expires (although it may be deleted from the cache
+     *                         to make place for other entries).
+     *
+     * @return bool TRUE if the entry was successfully stored in the cache, FALSE otherwise.
+     */
+    public function save($id, $data, $lifeTime = 0)
+    {
+        return $this->yiiCache->set($id, $data, $lifeTime);
+    }
+
+    /**
+     * Deletes a cache entry.
+     *
+     * @param string $id The cache id.
+     *
+     * @return bool TRUE if the cache entry was successfully deleted, FALSE otherwise.
+     *              Deleting a non-existing entry is considered successful.
+     */
+    public function delete($id)
+    {
+        return $this->yiiCache->delete($id);
+    }
+
+    /**
+     * Retrieves cached information from the data store.
+     *
+     * The server's statistics array has the following values:
+     *
+     * - <b>hits</b>
+     * Number of keys that have been requested and found present.
+     *
+     * - <b>misses</b>
+     * Number of items that have been requested and not found.
+     *
+     * - <b>uptime</b>
+     * Time that the server is running.
+     *
+     * - <b>memory_usage</b>
+     * Memory used by this server to store items.
+     *
+     * - <b>memory_available</b>
+     * Memory allowed to use for storage.
+     *
+     * @return array|null An associative array with server's statistics if available, NULL otherwise.
+     */
+    public function getStats()
+    {
+        return null;
+    }
+}
---
+++ src/Annotations.php
@@ -0,0 +1,76 @@
+<?php
+
+use Doctrine\Common\Annotations\AnnotationException;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\Annotations\CachedReader;
+use yii\base\InvalidConfigException;
+use yii\caching\CacheInterface;
+use yii\caching\FileCache;
+use yii\di\Instance;
+
+/**
+ * Class Annotations
+ */
+class Annotations
+{
+    /**
+     * @var string|CacheInterface
+     */
+    public $cache = false;
+
+    /**
+     * @var bool
+     */
+    public $debug = false;
+
+    /**
+     * @var string
+     */
+    public $path;
+
+    /**
+     * @var CachedReader
+     */
+    private $reader;
+
+    /**
+     *
+     */
+    protected const CACHE_PREFIX = 'annotations.';
+
+    /**
+     * Annotations constructor.
+     * @throws AnnotationException
+     * @throws InvalidConfigException
+     */
+    public function init(): void
+    {
+        if (method_exists(AnnotationRegistry::class, 'registerLoader')) {
+            AnnotationRegistry::registerLoader('class_exists');
+        }
+        $cacheComponent = is_string($this->cache) ? Instance::ensure($this->cache) : $this->cache;
+        if (!$cacheComponent) {
+            $fileCache = new FileCache();
+            $fileCache->cacheFileSuffix = static::CACHE_PREFIX;
+            if ($this->path !== null) {
+                $fileCache->cachePath = $this->path;
+            }
+            $cacheComponent = new AnnotationCache($fileCache);
+
+        }
+        $this->reader = new CachedReader(
+            new AnnotationReader(),
+            new AnnotationCache($cacheComponent),
+            $this->debug
+        );
+    }
+
+    /**
+     * @return CachedReader
+     */
+    public function getReader(): CachedReader
+    {
+        return $this->reader;
+    }
+}
