a69ec6e3280db4aa48af91f1c54f7a1da22b517c
d.egorov <d.egorov@smileexpo.ru>
2019-05-11 09:25:20 +0300

    add CI

---
+++ tests/Unit/AnnotationCacheTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace yii\annotations\test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AnnotationCacheTest
+ * @package yii\annotations\test\Unit
+ */
+class AnnotationCacheTest extends TestCase
+{
+
+    public function testFetch()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testContains()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testDelete()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test__construct()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testSave()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testGetStats()
+    {
+        $this->assertTrue(true);
+    }
+}
